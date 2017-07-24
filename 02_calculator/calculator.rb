9908a60f70ce010de9ed080f2611bcd248c9b63c
Sheldon <sheldonkostiuk@hotmail.com>
2017-07-23 23:40:07 -0700

    Wrote calculator.rb that passes tests, missed on last commit

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,17 @@
-#write your code here
+def add (number_one, number_two)
+	number_one + number_two
+end
+
+def subtract (number_one, number_two)
+	number_one - number_two
+end
+
+def sum(numbers)
+sum = 0
+numbers.each do |i|
+	sum = sum + i
+end
+sum.to_i
+end
+
+
