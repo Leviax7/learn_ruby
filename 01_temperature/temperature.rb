4f3bff2aee3429a1c4fd35e3ce621cb775d2cf4a
Sheldon <sheldonkostiuk@hotmail.com>
2017-07-23 20:40:40 -0700

    Finished first 3 problems All passing rspec tests

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
-#write your code here
+def ftoc temp
+	 cels = (((temp.to_f - 32) *5) / 9)
+end
+
+def ctof temp
+	fahr = (((temp.to_f*9)/5)+32)
+end
+
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,38 @@
-#write your code here
+def echo phrase
+	phrase
+end
+
+
+def shout phrase
+	phrase.upcase
+end
+
+def repeat phrase
+	"#{phrase} #{phrase}"
+end
+
+def repeat (phrase, times = 2)
+	("#{phrase} " * times.to_i).strip
+end
+
+def start_of_word (word, letter_number)
+	word.chars[0..letter_number - 1].join
+end
+
+def first_word phrase
+	a = phrase.split(' ')
+	a[0]
+end
+
+def titleize word
+	checks = ["And", "The", "Over"]	
+	a = word.split.map(&:capitalize)
+	a.each do |i|
+		if checks.include? i
+			i.downcase!
+		
+		end
+	end
+a[0].capitalize!
+a.join(' ')
+end
