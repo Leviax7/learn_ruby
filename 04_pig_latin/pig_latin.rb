0c493b9ad0a6c72a3c00ef4c9e06d43980b7d3a0
Sheldon <sheldonkostiuk@hotmail.com>
2017-07-26 02:33:42 -0700

    Methodized current code in case of needed repitition

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,9 +1,11 @@
-def translate pig_latin
-second_word = []
+
+def convert_two pig_latin
 vowels = ["a", "e", "i", "o", "u"]
 consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+second_word = []
 
-if pig_latin.split.length > 1
+ 
+ 
 	pig_latin = pig_latin.split(' ')
 	second_word << pig_latin.slice!(1)
 	pig_latin = pig_latin.join
@@ -14,7 +16,7 @@ if pig_latin.split.length > 1
 	consonants.cycle(pig_latin.length) do |x|
 		if pig_latin[0] == x
 			pig_latin.rotate!
-			redo
+			
 		end
 	end
 
@@ -24,14 +26,19 @@ if pig_latin.split.length > 1
 	consonants.cycle(second_word.length) do |x|
 		if second_word[0] == x
 			second_word.rotate!
-			redo
+			
 		end
 	end
 
 	second_word.push('ay')
 	pig_latin = pig_latin + second_word
 	pig_latin.join
-else	
+end	
+
+
+def convert pig_latin
+vowels = ["a", "e", "i", "o", "u"]
+consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
 	pig_latin = pig_latin.split('')
  
 
@@ -44,5 +51,12 @@ else
 	pig_latin.push("ay")
 	pig_latin.join
 end
+
+
+def translate pig_latin
+	if pig_latin.split.length > 1
+		convert_two(pig_latin)
+	else
+		convert(pig_latin)
 end
-puts translate("three")
+end
