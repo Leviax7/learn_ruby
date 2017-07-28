31751933b1ea57d9afa3d24c8ed8877e265d45fa
Sheldon <sheldonkostiuk@hotmail.com>
2017-07-28 00:52:32 -0700

    Passes all tests

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
-# write your code here
+attr_reader :title
+
+def title=(book_title)
+	exceptions =  ["And", "In", "The","Of", "A","An"]
+	@title = book_title.split(/(\W)/)
+	@title.map!(&:capitalize)
+	@title.drop(1).map do |x|
+		if exceptions.include?(x)
+			x.downcase!
+		end
+		
+	end
+@title = @title.join
+
 end
+end
+
