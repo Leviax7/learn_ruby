55259faaeb84162dc40d16a847cb998aa8897c19
Sheldon <sheldonkostiuk@hotmail.com>
2017-07-28 02:50:37 -0700

    Passes rspec tests

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,17 @@
 class Timer
-  #write your code here
+attr_reader :seconds
+
+def initialize
+@seconds=0
+@time = Time.gm(2017,nil,nil,0,0,0,0)
+end
+
+def seconds=(number)
+@time = @time + number
+end
+
+def time_string
+	
+	@time.strftime('%H:%M:%S')
+end
 end
